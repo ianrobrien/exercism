20788e91640deb4e192fea9502adac0ca57e4ab4
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-09-21 12:15:29 +0200

    Echo path

--- .github/bin/fetch-exercism.sh
+++ .github/bin/fetch-exercism.sh
@@ -25,4 +25,4 @@ fi
 
 # Extract the tar.gz file to the current directory
 mkdir -p bin && tar -xzvf "$filename" -C bin
-export PATH="${{ github.workspace }}/bin:$PATH"
+export PATH="$GITHUB_WORKSPACE/bin:$PATH"
