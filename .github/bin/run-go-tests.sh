e7d4b5a90b808986c50034c73f825821d3e22946
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-09-21 13:34:09 +0200

    Test tests

--- .github/bin/run-go-tests.sh
+++ .github/bin/run-go-tests.sh
@@ -15,9 +15,8 @@ golangci_lint_url="${golangci_lint_base_url}/v${golangci_lint_version}/${golangc
 # Function to download, verify, and extract a tar.gz file
 download_verify_extract() {
     local url="$1"
-    local version="$2"
-    local filename="$3"
-    local expected_hash="$4"
+    local filename="$2"
+    local expected_hash="$3"
 
     # Use curl to download the file
     curl -sSfL -o "$filename" "$url"
@@ -47,43 +46,47 @@ download_verify_extract() {
 
 # Function to lint Go source files in a directory
 lint_go_files() {
-  local dir="$1"
-  pushd "$dir" || exit 1
+  for exercise in go/*/; do
+    pushd "$exercise" || exit 1
 
-  # Use find to filter Go source code files for linting (exclude test files)
-  files_to_test=$(find . -type f -name "*.go" ! -name "*_test.go")
+    echo "$PWD"
 
-  # Run golangci-lint quietly and capture its exit code
-  if [ -n "$files_to_test" ]; then
-    if golangci-lint run "$files_to_test"; then
-      : # Do nothing on success
-    else
-      # If it fails, print the output and set exit_code to 1
-      echo "Linting failed in $(pwd):"
-      exit_code=1
+    # Use find to filter Go source code files for linting (exclude test files)
+    files_to_test=$(find . -type f -name "*.go" ! -name "*_test.go")
+
+    # Run golangci-lint quietly and capture its exit code
+    if [ -n "$files_to_test" ]; then
+      if golangci-lint run "$files_to_test"; then
+        : # Do nothing on success
+      else
+        # If it fails, print the output and set exit_code to 1
+        echo "Linting failed in $(pwd):"
+        exit_code=1
+      fi
     fi
-  fi
 
-  # Return to the original directory
-  popd || exit 1
+    # Return to the original directory
+    popd || exit 1
+  done
 }
 
 # Function to run "exercism test" in a directory
 run_exercism_test() {
-  local dir="$1"
-  pushd "$dir" || exit 1
+  for exercise in go/*/; do
+    pushd "$exercise" || exit 1
 
-  # Run "exercism test" and capture its exit code
-  if exercism test; then
-    : # Do nothing on success
-  else
-    # If it fails, print the output and set exit_code to 1
-    echo "Testing failed in $(pwd):"
-    exit_code=1
-  fi
+    # Run "exercism test" and capture its exit code
+    if exercism test; then
+      : # Do nothing on success
+    else
+      # If it fails, print the output and set exit_code to 1
+      echo "Testing failed in $(pwd):"
+      exit_code=1
+    fi
 
-  # Return to the original directory
-  popd || exit 1
+    # Return to the original directory
+    popd || exit 1
+  done
 }
 
 
@@ -91,19 +94,17 @@ run_exercism_test() {
 
 
 
-download_verify_extract "$exercism_url" "$exercism_version" "$exercism_filename" "$exercism_expected_hash"
-download_verify_extract "$golangci_lint_url" "$golangci_lint_version" "$golangci_lint_filename" "$golangci_lint_expected_hash"
+download_verify_extract "$exercism_url" "$exercism_filename" "$exercism_expected_hash"
+download_verify_extract "$golangci_lint_url" "$golangci_lint_filename" "$golangci_lint_expected_hash"
 
 # Initialize the exit code to 0
 exit_code=0
 
 # Iterate through all subdirectories of the "go" directory
-for exercise in go/*/; do
-  echo "lining all go files"
-  lint_go_files "$exercise"
-#  echo "running exercism tests"
-#  run_exercism_test "$exercise"
-done
+#  echo "linting all go files"
+#  lint_go_files "$exercise"
+echo "running exercism tests"
+run_exercism_test "$exercise"
 
 # Return the exit code at the end of the script
 exit $exit_code
