49c56fa3c86953c5bb72c7128bc8a2ac26f9807d
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-09-21 21:52:05 +0200

    Add bird-watcher solution

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -1,32 +1,51 @@
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
 
-    public BirdWatcher(int[] birdsPerDay) {
-        this.birdsPerDay = birdsPerDay.clone();
+  private final int[] birdsPerDay;
+
+  public BirdWatcher(int[] birdsPerDay) {
+    this.birdsPerDay = birdsPerDay.clone();
+  }
+
+  public int[] getLastWeek() {
+    return new int[]{0, 2, 5, 3, 7, 8, 4};
+  }
+
+  public int getToday() {
+    return birdsPerDay[birdsPerDay.length - 1];
+  }
+
+  public void incrementTodaysCount() {
+    birdsPerDay[birdsPerDay.length - 1]++;
+  }
+
+  public boolean hasDayWithoutBirds() {
+    for (int i = 0; i < birdsPerDay.length - 1; i++) {
+      if (birdsPerDay[i] == 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public int getCountForFirstDays(int numberOfDays) {
+    int sum = 0;
+    int days = numberOfDays < birdsPerDay.length
+        ? numberOfDays
+        : birdsPerDay.length;
+
+    for (int i = 0; i <= days - 1; i++) {
+      sum += birdsPerDay[i];
     }
 
-    public int[] getLastWeek() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getLastWeek() method");
-    }
+    return sum;
+  }
 
-    public int getToday() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getToday() method");
+  public int getBusyDays() {
+    int sum = 0;
+    for (int i = 0; i < birdsPerDay.length - 1; i++) {
+      sum += birdsPerDay[i] >= 5 ? 1 : 0;
     }
-
-    public void incrementTodaysCount() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
-    }
-
-    public boolean hasDayWithoutBirds() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
-    }
-
-    public int getCountForFirstDays(int numberOfDays) {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
-    }
-
-    public int getBusyDays() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
-    }
+    return sum;
+  }
 }
